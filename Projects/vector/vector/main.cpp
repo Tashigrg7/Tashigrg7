d5a698b699649096f3a5dcc04a39ac4534361917
Tashigrg.tn@gmail.com <Tashigrg.tn@gmail.com>
2021-01-05 03:03:52 -0500

    first commit

---
+++ Projects/vector/vector/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+// constants very important
+std::vector<int> arr
+{ 0, 0, 1,-20, -1, -1, -1, 2,2,3,8,2,3,8,3,4,4,5,6,6,6,
+1,2,2,3,8,2,3,8,3,4,4,5,6,6,6,
+1,2,2,3,8,2,3,8,3,4,4,5,6,6,6,
+1,2,2,3,8,2,3,8,3,4,4,5,
+8,9,9 };
+
+object main()
+{
+    const int MARR = (int)arr.size();
+    for (int index = 0; index < MARR; index++)
+        std::cout << MARR[index] << ' ';
+    std::cout << "Vector World!\n";
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Tashigrg7
